a1b110d87788282471a4e89ab0b5925552ef660a
alejandroalvarezdev <alejandroalvarezdev@gmail.com>
2021-12-17 10:27:59 -0600

    data import ok calculo y accesibilidad de propiedades ok

--- src/app/dashboards/pages/main/main.component.ts
+++ src/app/dashboards/pages/main/main.component.ts
@@ -9,7 +9,7 @@ import { CertificationsService } from 'src/app/services/certifications.service'
 export class MainComponent implements OnInit {
 
   resultados:any[] = [];
-
+  sumaComplete:number=0;
 
   constructor(private certificationService:CertificationsService) { }
 
@@ -19,11 +19,20 @@ export class MainComponent implements OnInit {
       // console.log(resp.kind_of_formation);
       this.resultados = resp.kind_of_formation;
       console.warn(this.resultados);
-      
-    })
 
+      //suma con el filtro de Status Complete
+      this.resultados.forEach(element => {
+        if (element.Status == 'Complete') {
+        this.sumaComplete = this.sumaComplete +1;
+        console.log(element.Person);
+        
+        
+        
+        }
+      });
     
-
+    })
+    
   }
-
+  
 }
